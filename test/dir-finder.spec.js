f43e113ea2925d596a3dd73001c08f2dbc4f04f6
Antonio Merker <antonio.merker@gmail.com>
2016-10-15 14:03:49 +0200

    added initial tests

---
+++ test/dir-finder.spec.js
@@ -0,0 +1,57 @@
+import test from 'ava';
+import mock from 'mock-fs';
+import findDir from '../src/dir-finder';
+
+test.before('prep', () => {
+  mock({ './Keep': {} });
+});
+
+test('dirList is missing', (t) => {
+  t.throws(() => {
+    findDir();
+  });
+});
+
+test('dirList has wrong type', (t) => {
+  t.throws(() => {
+    findDir('foo');
+  });
+});
+
+test('dirList is empty array', (t) => {
+  t.throws(() => {
+    findDir([]);
+  }, 'NODIR');
+});
+
+test('dirList contains empty string', (t) => {
+  t.throws(() => {
+    findDir(['']);
+  }, 'NODIR');
+});
+
+test('no dirs found', (t) => {
+  t.throws(() => {
+    findDir(['foo', '', 'bar']);
+  }, 'NODIR');
+});
+
+test('single existing dir', (t) => {
+  t.is(findDir(['./']), './');
+});
+
+test('mixed missing and existing dirs', (t) => {
+  t.is(findDir(['foo', './']), './');
+});
+
+test('multiple existing dirs', (t) => {
+  t.is(findDir(['./Keep', './']), './Keep');
+});
+
+test('different syntaxes for existing dir', (t) => {
+  t.is(findDir(['Keep', './Keep']), 'Keep');
+});
+
+test.after('cleanup', () => {
+  mock.restore();
+});
